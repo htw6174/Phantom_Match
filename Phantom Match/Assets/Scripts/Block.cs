9d5fc9d0cfcf365948cba5f12857339308ac12e9
Hardy Whitney <htw6174@gmail.com>
2016-04-18 17:58:32 -0400

    test to make sure .gitignore is working properly

---
+++ Phantom Match/Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Block : MonoBehaviour {
+
+
+
+    void Start()
+    {
+
+    }
+}
---
+++ Phantom Match/Assets/Scripts/BlockGrid.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGrid : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    private Block[,] blocks;
+
+    /// <summary>
+    /// Returns the Block at position
+    /// </summary>
+    /// <param name="x">Horizontal postion</param>
+    /// <param name="y">Vertical position</param>
+    /// <param name="clampRange">
+    /// true: returns closest valid block to x, y
+    /// false: reutrns null when x, y is outside of range
+    /// </param>
+    /// <returns></returns>
+    public Block GetBlock(int x, int y, bool clampRange = true)
+    {
+        if (clampRange)
+        {
+            x = Mathf.Clamp(x, 0, width - 1);
+            y = Mathf.Clamp(y, 0, height - 1);
+            return blocks[x, y];
+        }
+        else
+        {
+            if (x < 0 || x < 0 || x > width - 1 || x > height - 1)
+            {
+                return null;
+            }
+            else
+            {
+                return blocks[x, y];
+            }
+
+        }
+    }
+}
